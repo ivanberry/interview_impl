c6278066f80edd7c526b402338e07a7b4a97850c
ivanberry <tabvim.dev@gmail.com>
2020-05-17 11:18:46 +0800

    emulate fastclick simple

--- performance/300ms-delay.js
+++ performance/300ms-delay.js
@@ -1,8 +1,19 @@
 function detectClickEvent(e) {
 	console.log('event happend', e.type);
+	function test() {
+		console.log('emulate click');
+	}
 	if (e.type === 'touchend') {
 		e.preventDefault();
+		emulateClick(e, test);
 	}
+}
+
+function emulateClick(e, fn) {
+	const customE = new CustomEvent('niceClick');
+	e.target.removeEventListener('niceClick', fn, true);
+	e.target.addEventListener('niceClick', fn, true);
+	e.target.dispatchEvent(customE);
 }
 
 document.body.addEventListener('touchstart', detectClickEvent);
